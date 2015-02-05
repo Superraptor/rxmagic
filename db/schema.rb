470f52042fdf5f2dd929ba78f9b15eac491fa473
amf022 <arf56@pitt.edu>
2015-02-05 22:25:15 +0000

    db rake finally seems to have worked?

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130193006) do
+ActiveRecord::Schema.define(version: 20150205154904) do
 
   create_table "patients", force: :cascade do |t|
-    t.string   "firstName"
-    t.string   "lastName"
+    t.integer  "ptID"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "gender"
+    t.string   "dob"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
