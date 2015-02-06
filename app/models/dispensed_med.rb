f2134a8586d35c886fb6d82b750ff2decd88a932
amf022 <arf56@pitt.edu>
2015-02-06 19:11:26 +0000

    Migrated?

--- app/models/dispensed_med.rb
+++ app/models/dispensed_med.rb
@@ -1,5 +1,5 @@
 class DispensedMed < ActiveRecord::Base
-  has_one :patients
-  has_one :patients_ptID, :through => :patients, dependent: :nullify
+  has_one :patient
+  has_one :patients_ptID, :through => :patient, dependent: :nullify
   has_one :inventory_invID, :through => :inventory, dependent: :nullify
 end
