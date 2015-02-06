6a4191676e92deceb738e12d08aeb67c6f2b0284
amf022 <arf56@pitt.edu>
2015-02-06 19:35:30 +0000

    Inventory

--- app/models/dispensed_med.rb
+++ app/models/dispensed_med.rb
@@ -1,5 +1,6 @@
 class DispensedMed < ActiveRecord::Base
   has_one :patient
-  has_one :patients_ptID, :through => :patient, :source => :ptID, dependent: :nullify
-  has_one :inventory_invID, :through => :inventory, dependent: :nullify
+  has_one :patients_ptID, :through => :patient, :source => :dispensed_meds, dependent: :nullify
+  has_one :inventory
+  has_one :inventory_invID, :through => :inventory, :source => :dispensed_meds, dependent: :nullify
 end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,4 @@
 class Inventory < ActiveRecord::Base
-  belongs_to :dispensed_meds
+  belongs_to :dispensed_meds, foreign_key: "invID", class_name: "DispensedMed"
   has_many :medications_rxnorm_ndc, through: :medications_rx_norms, dependent: :nullify
 end
