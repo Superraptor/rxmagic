4b8dc864a32e1e426a06e8306439180a7c42f079
amf022 <arf56@pitt.edu>
2015-02-06 19:28:47 +0000

    Removed ',' syntax error

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ActiveRecord::Base
   belongs_to :applications
-  belongs_to :dispensed_meds
+  belongs_to :dispensed_meds, foreign_key: "ptID", class_name: "DispensedMed"
   belongs_to :prescriptions
 end
