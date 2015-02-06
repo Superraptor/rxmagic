757bb82134edd543caf9c71d1b22602f8312a138
amf022 <arf56@pitt.edu>
2015-02-06 19:02:07 +0000

    DispensedMed connection changed?

--- app/models/dispensed_med.rb
+++ app/models/dispensed_med.rb
@@ -1,4 +1,4 @@
 class DispensedMed < ActiveRecord::Base
-  has_one :patients_ptID, through: :patients, dependent: :nullify
-  has_one :inventory_invID, through: :inventories, dependent: :nullify
+  has_one :patients_ptID, through: :patients_id, dependent: :nullify
+  has_one :inventory_invID, through: :inventory, dependent: :nullify
 end
