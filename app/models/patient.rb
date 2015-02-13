3403515731c56d8d410e5fba9a63dcd421178a80
amf022 <arf56@pitt.edu>
2015-02-13 20:59:44 +0000

    We did some static pages

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -8,6 +8,15 @@ class Patient < ActiveRecord::Base
   has_many :prescription_patients
   has_many :prescriptions, :through => :prescription_patients
   
+  validates_uniqueness_of :ptid
+  validates :ptid, :numericality => { :greater_than_or_equal_to => 1 }
+  
+  validates_presence_of :firstname
+  validates_presence_of :lastname
+  validates_presence_of :gender
+  
+  validates_length_of :zip, minimum: 5, too_short: '. Please enter at least 5 characters.'
+  
   def self.search(search)
     search_condition = "%" + search + "%"
     find(:all, :conditions => ['firstname LIKE ? OR lastname LIKE ?', search_condition, search_condition])
